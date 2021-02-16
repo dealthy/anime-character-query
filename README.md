c4d0ded274f65ef237bc9f5edb5ad56fa1c1ebeb
Harry Yu <55175568+dealthy@users.noreply.github.com>
2021-02-16 13:00:27 +0800

    updatedatabase+planning

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 Further Development
  - search engine that allows filtering with search words
  - Most searched item collection, search history database 
- - sorting mechanism with a lot of categories
- - database with a lot of categories to allow searching to be more powerful 
+	- go over all the options listed in character (except characterid) and features table to search for keywords
+ - *family tree of the character
+ - insert picture of the character
+ - editing & browsing mode for the page
+ - create/delete a character
+ - sorting mechanism
+ - be more powerful 
  	- nicknames, famous quotes
  	- belonging anime -- linked to anime table
  	- linked characters + relationship
  	- ...
+ - 
  - info page that display database data (try to imitate Moegirlpedia / Wikipedia style)
+ - *black bars to block 
 
 
- Setting https://github.com/moegirlwiki/ (a chinese online anime character wikipedia site) as the developing model
+ Setting https://github.com/moegirlwiki/ (a chinese online anime character "wikipedia" site) as the developing model
--- animeCharacterQuery/src/animeCharacterQuery/infoPage.java
+++ animeCharacterQuery/src/animeCharacterQuery/infoPage.java
@@ -6,11 +6,23 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 
 public class infoPage extends JFrame {
 
 	private JPanel contentPane;
-
+	private JTextField searchWord;
+	infoPage frame = new infoPage();
 	/**
 	 * Launch the application.
 	 */
@@ -34,9 +46,29 @@ public class infoPage extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 650, 800);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setOpaque(false);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-	}
+		
+		searchWord = new JTextField();
+		searchWord.setEditable(false);
+		searchWord.setBounds(423, 6, 197, 31); 
+		searchWord.setColumns(10);
+		
+		JLabel pageTitle = new JLabel("Page Title");
+		pageTitle.setFont(new Font("Courier", Font.PLAIN, 20));
+		pageTitle.setBounds(24, 49, 599, 36);
+		contentPane.add(pageTitle);
+		
+		JLabel divider = new JLabel("_____________________________________________________________________________________");
+		divider.setForeground(Color.BLACK);
+		divider.setBounds(24, 69, 596, 16);
+		contentPane.add(divider);
 
+		//image component added with manual code
+		frame.add(new JLabel(new ImageIcon("Path/To/Your/Image.png")));
+
+	}
 }
